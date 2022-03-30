40a68d096e5a48b085fbc719cf2d27f01ac1a0f6
Wwiktoria1 <100830693+Wwiktoria@users.noreply.github.com>
2022-03-30 16:47:45 +0000

    .

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
+
 #include <stdio.h>
-#define PETLA1(x,n) for(x=n;x>n;x--)
-int main(void) {
-  printf("%d",PETLA1(8,3));
+#define LOOP(x, n)                  \
+  for (int i=(n-1); i>=0; i--) { \
+    printf("%d\n", i);                    \
+  }
+  int main(void){
+      LOOP(2,6);
+  
   return 0;
 }
